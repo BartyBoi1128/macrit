e1c28f5f86405cdfc43ba6b5e64701db0d636118
Adamos991 <19265808@studentmail.ul.ie>
2022-10-27 15:56:33 +0100

    feat: :ambulance: Finished navbar functionality.

--- macrit/reactapp/src/components/navbar.jsx
+++ macrit/reactapp/src/components/navbar.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../navbar.css";
 
-function Navbar(props) {
+function Navbar() {
+    const [active, setActive] = useState("nav_menu");
+    const [toggleIcon, setToggleIcon] = useState("nav_toggle");
+    const navSwitch = () => {
+        active === "nav_menu" ? setActive("nav_menu nav_active") : setActive("nav_menu");
+        toggleIcon === "nav_toggle" ? setToggleIcon("nav_toggle toggle") : setToggleIcon("nav_toggle");
+    }
     return (
         <nav className="nav">
             <a href="#" className="brand">macrit</a>
-            <ul className="nav_menu">
+            <ul className={active}>
                 <li className="nav_item">
                     <a href="#" className="nav_link">
                         Recipes
@@ -18,11 +24,16 @@ function Navbar(props) {
                 </li>
                 <li className="nav_item">
                     <a href="#" className="nav_link">
+                        Profile
+                    </a>
+                </li>
+                <li className="nav_item">
+                    <a href="#" className="nav_link">
                         Settings
                     </a>
                 </li>
             </ul>
-            <div className="nav_toggle">
+            <div onClick = {navSwitch} className={toggleIcon}>
                 <div className="line1"></div>
                 <div className="line2"></div>
                 <div className="line3"></div>
