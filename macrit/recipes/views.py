23ed98b04374b7577ce2d092b23ae29b9fca9b65
ULSean <sean26oneill@gmail.com>
2022-11-15 11:14:28 +0000

    updating recipe branch

--- macrit/recipes/views.py
+++ macrit/recipes/views.py
@@ -1,5 +1,6 @@
+import datetime
 from recipes.forms import UserCreationForm
-from django.shortcuts import HttpResponse, redirect, render
+from django.shortcuts import redirect, render
 from django.template import loader
 from recipes.models import User, Recipe
 from recipes.service import *
@@ -9,8 +10,9 @@ from django.core.exceptions import ValidationError
 
 # Create your views here.
 
-# testUser = User.objects.create(password="safepass", email="useremail@gmail.com")
-# testProfile = Profile.objects.create(user=testUser, first_name="Ivanna", second_name="Tinkle", height=183, weight=250,BMI=bmiCalc(Profile.height,Profile.weight),age=22,gender=False,weight_goal=200,weight_goal_time="12/12/2022",vegeterian=True, vegan=False)
+# testUser = User.objects.get(userid=19269609)
+# testProfile = Profile.objects.create(user=testUser, first_name="Ivanna", second_name="Tinkle", height=183, weight=250,BMI=bmiCalc(183, 250),age=22,gender=False,weight_goal=200,weight_goal_time=datetime.datetime(2022, 12, 12),vegeterian=True, vegan=False)
+
 
 def index(request):
     return render(request, 'index.html', {})
