e19dd7b433fb7a7e3a66a9cd8530811948029d5b
ULSean <sean26oneill@gmail.com>
2022-12-02 22:33:16 +0000

    Fixes

--- macrit/recipes/urls.py
+++ macrit/recipes/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('diary', views.diary, name='diary'),
     path('settings',views.settings, name='settings'),
     path('registerProfile', views.registerProfile, name='registerProfile'),
+    path('subscribe', views.subscribe, name='subscribe')
 ]
